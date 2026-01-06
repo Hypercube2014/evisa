29319e8e05dfc34596514c5fca46389bf63ac56e
Hypercube2014 <contact@hypercube.dj>
2026-01-06 10:21:48 +0300

    commit message

---
+++ evisa-applicant-api/classes/static/assets/repeated/sidebarmenuSIMO.js
@@ -0,0 +1,84 @@
+document.write("<!-- BEGIN SIDEBAR -->");
+document.write("		<div class='page-sidebar navbar-collapse collapse'>");
+
+document.write("                    <div class='user-panel text-center'>");
+document.write("                        <div class='image'>");
+document.write("                            <img src='assets/img/avatar1_small.jpg' class='img-circle' alt='User Image'>");
+document.write("                        </div>");
+document.write("                        <div class='info'>");
+document.write("                           <p>Bob Nilson</p>");
+document.write("                            <a href='applicationsListSIMO.html' style='color:#ffffff;'><i class='fa fa-circle text-success'></i>");
+document.write("                            Immigration Manager</a>");
+document.write("                        </div>");
+document.write("                    </div> ");
+
+
+document.write("			<!-- BEGIN SIDEBAR MENU -->        ");     
+document.write("			<ul class='page-sidebar-menu'>");
+
+document.write("				<li class='start active '>");
+document.write("					<a href='applicationsListSIMO.html'>");
+document.write("					<i class='fa fa-home'></i> ");
+document.write("					<span class='title'>Dashboard</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='searchApplicationsSIMO.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>eVisa Processing</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='detachApplications.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Detach Applications</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='agentSuspensionSIMO.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Suspend Agent</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='manageDM.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Manage Agents</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='PerformanceReportsSIMO.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Performance Reports</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='turnaroundtimeReportsSIMO.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Process Reports</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li class=''>");
+document.write("					<a href='AgentTrackerSIMO.html'>");
+document.write("					<i class='fa fa-book'></i> ");
+document.write("					<span class='title'>Agents Tracker</span>");
+document.write("					</a>");
+document.write("				</li>");
+
+document.write("				<li>");
+document.write("					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->");
+document.write("					<div class='sidebar-toggler hidden-phone'></div>");
+document.write("					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->");
+document.write("				</li>");
+
+document.write("			</ul>");
+document.write("			<!-- END SIDEBAR MENU -->");
+document.write("		</div>");
+document.write("		<!-- END SIDEBAR -->");
